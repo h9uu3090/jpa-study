a9b711fb2e7f173021d4fc59b0dba5d7aacab008
hkkim85 <hkkim85@icloud.com>
2024-12-31 23:54:26 +0900

    git init

---
+++ src/main/java/h9uu/jpa_study/JpaStudyApplication.java
@@ -0,0 +1,13 @@
+package h9uu.jpa_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaStudyApplication.class, args);
+	}
+
+}
